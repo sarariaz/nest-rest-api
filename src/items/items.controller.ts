4c740afb6b9ef69b3b1979c9916cc48a1cd44a0a
sara riaz <srq147147@gmail.com>
2019-09-25 09:11:41 -0700

    nest-rest-api

---
+++ src/items/items.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get , Post, Put, Delete} from '@nestjs/common';
+
+@Controller('items')
+export class ItemsController {
+    @Get()
+    findAll(): string{
+        return 'Get all items'
+    }
+@Post()
+create(): string{
+    return 'Create items'
+}
+
+}
