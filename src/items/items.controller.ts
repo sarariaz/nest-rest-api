8ab9269640d5aef43e07e8a045650058318dba82
sara riaz <srq147147@gmail.com>
2019-09-26 10:03:54 -0700

    nest-rest-api

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,35 +1,72 @@
-import { Controller, Get , Post, Put, Delete, Body, Param} from '@nestjs/common';
-import { CreateItemDto} from './dto/create-item.dto';
-import { publicDecrypt } from 'crypto';
+import { Controller, Get , Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
+//@Body works like req.body
 
 @Controller('items')
 export class ItemsController {
+    constructor( private readonly itemsService: ItemsService) {}
+
     @Get()
-    findAll(): string{
-        return 'Get all items'
+    async findAll() : Promise<Item[]> {
+        return this.itemsService.findAll();
     }
 
-@Get(':id')
-findOne(@Param('id') id : string){
-    return `Item : ${id}`
+    // @Get(':id')
+    // findOne(@Param() param){
+    //     return `Item ${param.id}`
+    // }
+    @Get(':id')
+    findOne(@Param('id') id) : Promise<Item>{
+        return this.itemsService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() createItemDto : CreateItemDto) : Promise<Item> {
+        return this.itemsService.create(createItemDto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id) : Promise<Item> {
+        return this.itemsService.delete(id);
+    }
+
+    @Put(':id')
+    update(@Body() updateItemDto : CreateItemDto , @Param('id') id) : Promise<Item> {
+        return this.itemsService.update(id ,updateItemDto)
+    }
 }
 
 
-@Post()
-create(@Body() createitemDto : CreateItemDto): string{
-    return `  Name : ${createitemDto.name}`
-
-}
-
-@Delete(':id')
-delete(@Param('id') id : string){
-    return `item ${id} deleted`
-}
-
-@Put(':id')
-update(@Body() updateitemDto : CreateItemDto, @Param('id') id): string{
-return `id: ${id}, name: ${updateitemDto.name}`
-}
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+// import { Controller, Get , Post, Put, Delete, Body , Req, Res } from '@nestjs/common';
+// import { CreateItemDto } from './dto/create-item-dto';
+// import { Request , Response } from 'express';
+// //@Body works like req.body
+
+// @Controller('items')
+// export class ItemsController {
+//     @Get()
+//     findAll(@Req() req:Request , @Res() res:Response) : Response {
+//         console.log(req.url);
+//         return res.send('Hello World');
+//     }
+
+//     @Post()
+//     create(@Body() createItemDto : CreateItemDto) : string {
+//         return `Name : ${createItemDto.name} Desc : ${createItemDto.description}`;
+//     }
+// }
